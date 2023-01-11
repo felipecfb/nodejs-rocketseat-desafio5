4b45da658ba3d2f07e7c127f59ef102525000f03
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-01-11 15:12:40 -0300

    ✨ feat: finalizando desafio

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,28 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
+    const paramFormatted = param.toLowerCase();
+
+    console.log(param);
+
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .where("LOWER(games.title) like LOWER(:title)", { title: `%${param}%`})
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      'SELECT COUNT(*) FROM games'
+    ); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
       // Complete usando query builder
   }
 }
